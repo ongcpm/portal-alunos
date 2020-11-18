313928bc0e34925822e4e5c4b975ccbed123416f
caiolavos <lavos98@gmail.com>
2020-11-18 06:07:51 -0300

    adicionando rotas de usuario

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,6 +30,52 @@ app.get("/", (req, res) => {
     res.send('PORTAL DE ALUNOS')
     console.log("Hello MEAN Soldier...Ready For Battle??");
 });
+
+app.get("/user/login", (req, res) => {
+    const login = {
+		email: req.body.email,
+		password: req.body.password
+	}
+	try {
+		let user = await User.findOne({
+			email: login.email
+		});
+		//check if user exit
+		if (!user) {
+			res.status(400).json({
+				type: "Not Found",
+				msg: "Wrong Login Details"
+			})
+		}
+		let match = await user.compareUserPassword(login.password, user.password);
+		if (match) {
+			let token = await user.generateJwtToken({
+				user
+			}, "secret", {
+				expiresIn: 604800
+			})
+			if (token) {
+				res.status(200).json({
+					success: true,
+					token: token,
+					userCredentials: user
+				})
+			}
+		} else {
+			res.status(400).json({
+				type: "Not Found",
+				msg: "Wrong Login Details"
+			})
+		}
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			type: "Something Went Wrong",
+			msg: err
+		})
+	}
+})
+
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`);
 });
